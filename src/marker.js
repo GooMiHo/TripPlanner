fb76b36b04d43bfd5394c7c94ef881e528d1e416
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-09-25 17:31:46 -0400

    markers work

--- src/marker.js
+++ src/marker.js
@@ -2,21 +2,30 @@
 const mapboxgl = require("mapbox-gl");
 
 const iconURLs = {
-    hotels: "http://i.imgur.com/D9574Cu.png",
-    restaurants: "http://i.imgur.com/cqR6pUI.png",
-    activities: "http://i.imgur.com/WbMOfMl.png"
+    hotels: "url(http://i.imgur.com/D9574Cu.png)",
+    restaurants: "url(http://i.imgur.com/cqR6pUI.png)",
+    activities: "url(http://i.imgur.com/WbMOfMl.png)"
 };
 
 const hotelElem = document.createElement('div');
-hotelElem.className = 'icons';
+// hotelElem.className = 'icons';
+hotelElem.style.width = "30px";
+hotelElem.style.height = "30px";
+hotelElem.style.backgroundSize = "contain";
 hotelElem.style.backgroundImage = iconURLs.hotels;
 
 const restElem = document.createElement('div');
-restElem.className = 'icons';
+// restElem.className = 'icons';
+restElem.style.width = "30px";
+restElem.style.height = "30px";
+restElem.style.backgroundSize = "contain";
 restElem.style.backgroundImage = iconURLs.restaurants;
 
 const actElem = document.createElement('div');
-actElem.className = 'icons';
+// actElem.className = 'icons';
+actElem.style.width = "30px";
+actElem.style.height = "30px";
+actElem.style.backgroundSize = "contain";
 actElem.style.backgroundImage = iconURLs.activities;
 
 
@@ -25,12 +34,11 @@ const buildMarker = (type, coordinates ) => {
 
     if(type === 'hotels'){
      newIcon= new mapboxgl.Marker(hotelElem);
-    } else if ( type ==='restaurants'){
+    } else if ( type === 'restaurants'){
         newIcon = new mapboxgl.Marker(restElem);
     }
 
     return newIcon.setLngLat(coordinates);
-
 }
 
 module.exports = buildMarker;
