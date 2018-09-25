3fba41de19506ba0985ab0e307efd661dfcbe32b
Ayema Chowdhury <ayema.chowdhury@gmail.com>
2018-09-25 17:02:11 -0400

    hello

---
+++ src/index.js
@@ -0,0 +1,23 @@
+// console.log('hello world');
+
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker")
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXllbWFjIiwiYSI6ImNqbWk1MnRhczAxY3kzcXFsNmdkZGpiNnkifQ.AP-AtdHq2H8gO6gOwWQdUQ';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+
+const markerDomEl = document.createElement("div");
+markerDomEl.style.width = "45px";
+markerDomEl.style.height = "45px";
+markerDomEl.style.backgroundImage = "url(https://image.ibb.co/kwTjSU/Dan.png)";
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+buildMarker("activity", [-74.009, 40.705]).addTo(map);
+
---
+++ src/marker.js
@@ -0,0 +1,36 @@
+
+const mapboxgl = require("mapbox-gl");
+
+const iconURLs = {
+    hotels: "http://i.imgur.com/D9574Cu.png",
+    restaurants: "http://i.imgur.com/cqR6pUI.png",
+    activities: "http://i.imgur.com/WbMOfMl.png"
+};
+
+const hotelElem = document.createElement('div');
+hotelElem.className = 'icons';
+hotelElem.style.backgroundImage = iconURLs.hotels;
+
+const restElem = document.createElement('div');
+restElem.className = 'icons';
+restElem.style.backgroundImage = iconURLs.restaurants;
+
+const actElem = document.createElement('div');
+actElem.className = 'icons';
+actElem.style.backgroundImage = iconURLs.activities;
+
+
+const buildMarker = (type, coordinates ) => {
+ let newIcon = new mapboxgl.Marker(actElem);
+
+    if(type === 'hotels'){
+     newIcon= new mapboxgl.Marker(hotelElem);
+    } else if ( type ==='restaurants'){
+        newIcon = new mapboxgl.Marker(restElem);
+    }
+
+    return newIcon.setLngLat(coordinates);
+
+}
+
+module.exports = buildMarker;
